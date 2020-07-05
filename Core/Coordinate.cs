5edfd168c173af4d752543d552a6c02483ad8d62
Wai Yan Myint Mo <dragonkey100@gmail.com>
2020-07-05 21:01:53 +0630

    initial commit

---
+++ Core/Coordinate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public readonly struct Coordinate
+    {
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int X { get; }
+        public int Y { get; }
+    }
+}
